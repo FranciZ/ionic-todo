665b0fa7916faaffa292cf87cdf0ab07408f3183
FranciZ <franci@proxima.si>
2016-05-09 17:43:06 +0200

    first commit

---
+++ www/js/main-ctrl.js
@@ -0,0 +1,75 @@
+
+angular.module('starter').controller('MainCtrl', function(
+  $scope,
+  $ionicModal,
+  todoService
+){
+
+  $scope.todoModel = {};
+  $scope.todos = todoService.model.list;
+
+  function init(){
+
+    todoService.getTodos();
+
+  }
+
+  init();
+
+  $scope.languages = {
+
+    'Slovenski':{
+      submit:'Dodaj',
+      delete:'Zbrisi',
+      selectLanguage:'Izberi jezik'
+    },
+    'English':{
+      submit:'Submit',
+      delete:'Delete',
+      selectLanguage:'Select language'
+    },
+    'Deutsch':{
+      submit:'Schubmit',
+      delete:'Zbrisi',
+      selectLanguage:'Schelect language'
+    }
+
+  };
+
+  $scope.addTodoClick = function(){
+
+    // { title:'My to do'}
+    todoService.addTodo($scope.todoModel);
+
+  };
+
+  $scope.removeTodoClick = function(id){
+
+    todoService.removeTodo(id);
+
+  };
+
+  $scope.setLanguage = function(translations){
+
+    $scope.locales.ui = translations;
+    $scope.modal.hide();
+
+  };
+
+  $scope.locales = {
+    ui : $scope.languages.Slovenski
+  };
+
+  $scope.onLanguageClick = function(){
+
+    $ionicModal.fromTemplateUrl('my-modal.html', {
+      scope: $scope,
+      animation: 'slide-in-up'
+    }).then(function(modal) {
+      $scope.modal = modal;
+      $scope.modal.show();
+    });
+
+  };
+
+});
---
+++ www/js/todo-service.js
@@ -0,0 +1,91 @@
+angular.module('starter').factory('todoService', function($localForage){
+
+  var todoService = {
+
+    model:{
+      list:[]
+    },
+    addTodo:function(todo){
+
+      todo.id = guid();
+
+      // { title:'moj title', id:'as0102idhad8912'}
+
+      $localForage.getItem('todos')
+        .then(function(todos){
+
+          // if todos does not exist create array
+          if(!todos) todos = [];
+
+          // push new to do to array
+          todos.push(todo);
+
+          // save the todos array back to localstorage
+          return $localForage.setItem('todos', todos);
+
+        })
+        .then(function(todos){
+
+          // instead of updating the reference we need to extend
+          // the current list with the new one using angular.extend
+          // todoService.model.list = todos; // WRONG
+
+          angular.extend(todoService.model.list,todos);
+
+        });
+
+    },
+    getTodos:function(){
+
+      $localForage.getItem('todos')
+        .then(function(todos){
+
+          angular.extend(todoService.model.list,todos);
+
+        });
+
+    },
+    /**
+     * Accepts id of the to-do you want to remove
+     * @param id
+     */
+    removeTodo:function(id){
+
+      $localForage.getItem('todos')
+        .then(function(todos){
+
+          angular.forEach(todos, function(todo, i){
+
+            if(todo.id === id){
+              todos.splice(i,1);
+            }
+
+          });
+
+          angular.forEach(todoService.model.list, function(todo, i){
+
+            if(todo.id === id){
+              todoService.model.list.splice(i,1);
+            }
+
+          });
+
+          return $localForage.setItem('todos', todos)
+            .then(function(todos){
+
+            });
+
+        });
+
+    },
+    updateTodo:function(){
+
+
+
+    }
+
+  };
+
+  return todoService;
+
+});
